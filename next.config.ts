19fc9ce246da155c1b3d11040a20c1f2f0c57481
Appon Islam <appon2003@gmail.com>
2026-07-14 00:22:16 +0600

    implement restaurant schedule configuration page and update configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.10.7.111'],
+  allowedDevOrigins: ['10.10.7.111', 'vibez.apponislam.top'],
 };
 
 export default nextConfig;
